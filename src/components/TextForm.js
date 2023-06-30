a6f5557c35a17c5bc0d6b25d4426f9fbedc56735
mahuyaghosh396 <105850543+mahuyaghosh396@users.noreply.github.com>
2023-06-30 18:31:35 +0530

    on change and on click

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,7 +2,16 @@ import React, {useState} from 'react'
 
 export default function TextForm(prop) {
     const handleUpcase = ()=>{
-        setText("you have clicked on handleUPclick")
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+
+// a function which listen a even when any changes occure in text area
+//onChange is usefull for text area otherwise we can not get permission to write or update in text area
+
+    const handleOnChange = (event)=>{
+       console.log("on change")
+       setText(event.target.value)
     }
     const [text, setText] = useState("Enter text here");
 
@@ -10,7 +19,7 @@ export default function TextForm(prop) {
    <><h1>{prop.handling}</h1>
    <div className="mb-3">
           <label htmlFor="myBox" className="form-label">Enter text</label>
-          <textarea className="form-control" value={text} id="myBox" rows="8"></textarea>
+          <textarea className="form-control"  onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
       </div>
       <button className="btn btn-primary" onClick={handleUpcase}>convert to upper case</button>
     </>
