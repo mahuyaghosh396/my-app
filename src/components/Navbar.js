50c3da1b10ea1f0a36b1e1b7f788ad2fe9e1a3d0
mahuyaghosh396 <105850543+mahuyaghosh396@users.noreply.github.com>
2023-06-30 12:32:54 +0530

    concept of props

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">Navbar</a>
+        <a className="navbar-brand" href="/">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -15,7 +15,7 @@ export default function Navbar() {
             </li>
 
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">About</a>
+              <a className="nav-link active" aria-current="page" href="/">{props.about}</a>
             </li>
 
             <li className="nav-item">
